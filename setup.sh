9580c4536a29d0cad49c55a959d381daaa8fd936
Colm Talbot <talbotcolm@gmail.com>
2026-04-21 11:49:36 -0400

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+conda create -y -n bilby python=3.12 bilby_pipe ipykernel lalframe
+conda activate bilby
+python -m ipykernel install --user --name bilby --display-name "Bilby"
+
+cd single-event-inference/data
+wget https://gwosc.org/eventapi/html/GWTC-2.1-confident/GW190425/v3/L-L1_GWOSC_16KHZ_R1-1240213455-4096.gwf
+wget https://gwosc.org/eventapi/html/GWTC-2.1-confident/GW190425/v3/V-V1_GWOSC_16KHZ_R1-1240213455-4096.gwf
+cd -
