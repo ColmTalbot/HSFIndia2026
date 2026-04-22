604627a41c8e8ec61bfff44ef49ca5c11d4c6ad8
Colm Talbot <talbotcolm@gmail.com>
2026-04-22 08:57:42 +0530

    add sarathi specific directions to setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,14 @@ conda activate bilby
 python -m ipykernel install --user --name bilby --display-name "Bilby"
 
 cd single-event-inference/data
-wget https://gwosc.org/eventapi/html/GWTC-2.1-confident/GW190425/v3/L-L1_GWOSC_16KHZ_R1-1240213455-4096.gwf
-wget https://gwosc.org/eventapi/html/GWTC-2.1-confident/GW190425/v3/V-V1_GWOSC_16KHZ_R1-1240213455-4096.gwf
+if [ -f /home/colm.talbot/HSFIndia2026/single-event-inference/data/L-L1_GWOSC_16KHZ_R1-1240213455-4096.gwf ]; then
+   cp /home/colm.talbot/HSFIndia2026/single-event-inference/data/L-L1_GWOSC_16KHZ_R1-1240213455-4096.gwf ./
+else
+   wget https://gwosc.org/eventapi/html/GWTC-2.1-confident/GW190425/v3/L-L1_GWOSC_16KHZ_R1-1240213455-4096.gwf
+fi
+if [ -f /home/colm.talbot/HSFIndia2026/single-event-inference/data/V-V1_GWOSC_16KHZ_R1-1240213455-4096.gwf ]; then
+   cp /home/colm.talbot/HSFIndia2026/single-event-inference/data/V-V1_GWOSC_16KHZ_R1-1240213455-4096.gwf ./
+else
+   wget https://gwosc.org/eventapi/html/GWTC-2.1-confident/GW190425/v3/V-V1_GWOSC_16KHZ_R1-1240213455-4096.gwf
+fi
 cd -
